4699ba890e42f3c8afa4fed60ff0227b711ab8dc
blendthink <admin@blendthink.dev>
2023-03-16 09:07:39 +0900

    Remove unused scripts/install-flutter.sh

--- scripts/install-flutter.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-FLUTTER_TAG="3.6.0-1.0.pre"
-git clone https://github.com/flutter/flutter.git --depth 1 -b "$FLUTTER_TAG" ".flutter/flutter_sdk"
-
-.flutter/flutter_sdk/bin/flutter --version
